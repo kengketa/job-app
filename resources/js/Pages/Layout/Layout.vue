7c75ce0decb9b11f2b2cea5aff11128e787a8a75
Kengketa <kengketa@gmail.com>
2024-02-06 22:00:13 +0700

    refactor: use layout for index page

---
+++ resources/js/Pages/Layout/Layout.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="bg-white min-h-screen">
+        <Nav/>
+        <section id="content" class="mt-20 text-gray-800 z-0">
+            <main>
+                <slot></slot>
+            </main>
+            <footer>
+                <div class="bg-gray-100 h-60 flex justify-evenly px-10 py-4 items-center">
+                    <div>กองทรัพยากรมนุษย์
+                        มหาวิทยาลัยศิลปากร
+                    </div>
+                    <div>เบอร์โทรศัพท์
+                        กองทรัพยากรมนุษย์ และหน่วยงานภายใน
+                        งานพัฒนาทรัพยากรมนุษย์
+                        โทร. 02-849-7567, 02-849-7544
+                        งานบริหารทรัพยากรมนุษย์
+                        โทร. 02-849-7546, 02-849-7547
+                    </div>
+                    <div>กองทรัพยากรมนุษย์
+                        ตึกสำนักงานอธิการบดี (ตลิ่งชัน)
+                    </div>
+                </div>
+                <div class="w-full h-10 bg-blue-800 text-white flex items-center px-20">
+                    <p>2023 Copyright มหาวิทยาลัยศิลปากร</p>
+                </div>
+            </footer>
+        </section>
+    </div>
+</template>
+
+<script>
+import Nav from "@/Pages/Layout/Nav.vue";
+
+export default {
+    name: "Layout",
+    components: {Nav},
+    data() {
+        return {};
+    },
+};
+</script>
+
+<style scoped>
+</style>
