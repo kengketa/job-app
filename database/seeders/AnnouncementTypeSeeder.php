0622d0cd492d8117b10f174ad1723ca97c343503
kthitirat <kthitirat@aru.ac.th>
2024-02-22 20:37:35 +0700

    feat: add AnnouncementType Model

---
+++ database/seeders/AnnouncementTypeSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\AnnouncementType;
+
+class AnnouncementTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        AnnouncementType::factory()->create([
+            'name' => 'สายวิชาการ'
+        ]);
+
+        AnnouncementType::factory()->create([
+            'name' => 'สายสนับสนุน'
+        ]);
+    }
+}
