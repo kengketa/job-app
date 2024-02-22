c5d9e162366b3eeb82ca0270c0768941b1b704f8
kthitirat <kthitirat@aru.ac.th>
2024-02-22 21:01:51 +0700

    feat: add AnnouncementCategory

---
+++ app/Models/AnnouncementCategory.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AnnouncementCategory extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name'];
+    protected $hidden = ['create_at', 'updated_at'];
+}
