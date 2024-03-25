6907de035a1d3483661e8dfde33ee273cd23ecaa
kthitirat <kthitirat@aru.ac.th>
2024-03-25 15:01:12 +0700

    feat: add model applicat not yet done

---
+++ app/Models/Applicant.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Applicant extends Model
+{
+    use HasFactory;
+    use InteractsWithMedia;
+
+    public const MEDIA_COLLECTION_DOCUMENTS = 'documents';
+
+    protected $fillable = [
+        'user_id',
+        'announcement_id',
+        'application_number',
+        'photo',
+        'prefix',
+        'first_name',
+        'last_name',
+        'position',
+        'address',
+        'birthdate',
+        'age',
+        'phone',
+        'birthplace',
+        'ethnicity',
+        'email',
+        'citizen_id',
+        'marital_status',
+        'nationality',
+        'district',
+        'province',
+        'card_issued_date',
+        'expiration_date',
+        'military_service',
+        'religion',
+        'current_occupation',
+        'reason_for_leaving',
+        'education_and_training_year',
+        'school_name',
+        'education_degree',
+        'grade',
+        'additional_course',
+        'training',
+        'work_experience',
+        'organization_name',
+        'position_duties',
+        'salary',
+        'resignation_reason',
+        'achievements',
+        'experience_gained',
+        'talent',
+        'reference_person_name',
+        'reference_person_lastname',
+        'current_position',
+        'workplace_and_phone',
+        'relationship',
+        'signature_applicant',
+        'submit_date',
+        'signature_recruiter',
+        'recruiter_date',
+        'signature_payee',
+        'payee_date',
+        'accepted',
+      
+    ];
+    protected $casts = [
+        'create_at' => 'date',
+        'updated_at' => 'date',
+    ];
+
+    public function type()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Announcement::class, 'announcement_id');
+    }
+
+
+    public function scopeFilter(Builder $query, array $filters): void
+    {
+        if (isset($filters['user_id']) && $filters['user_id'] != null) {
+            $query->where('user_id', $filters['user_id']);
+        }
+        if (isset($filters['announcement_id']) && $filters['announcement_id'] != null) {
+            $query->where('announcement_id', $filters['announcement_id']);
+        }
+    }
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection(self::MEDIA_COLLECTION_DOCUMENTS);
+    }
+
+}
---
+++ database/factories/ApplicantFactory.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Applicant;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Applicant>
+ */
+class ApplicantFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::inRandomOrder()->first()->id,
+            'announcement_id' => Announcement::inRandomOrder()->first()->id,           
+            'application_number' => $this->faker->numberBetween(1, 20),
+            'photo' => $this->faker->sentence,
+            'prefix' => $this->faker->sentence,
+            'first_name' => $this->faker->sentence,
+            'last_name' => $this->faker->sentence,
+            'position' => $this->faker->sentence,
+            'address' => $this->faker->sentence,
+            'birthdate' => $this->faker->sentence,
+            'age' => $this->faker->sentence,
+            'phone' => $this->faker->sentence,
+            'birthplace' => $this->faker->sentence,
+            'ethnicity' => $this->faker->sentence,
+            'email' => $this->faker->sentence,
+            'citizen_id' => $this->faker->sentence,
+            'marital_status' => $this->faker->sentence,
+            'nationality' => $this->faker->sentence,
+            'district' => $this->faker->sentence,
+            'province' => $this->faker->sentence,
+            'card_issued_date' => $this->faker->sentence,
+            'expiration_date' => $this->faker->sentence,
+            'military_service' => $this->faker->sentence,
+            'religion' => $this->faker->sentence,
+            'current_occupation' => $this->faker->sentence,
+            'reason_for_leaving' => $this->faker->sentence,
+            'education_and_training_year' => $this->faker->sentence,
+            'school_name' => $this->faker->sentence,
+            'education_degree' => $this->faker->sentence,
+            'grade' => $this->faker->sentence,
+            'additional_course' => $this->faker->sentence,
+            'training' => $this->faker->sentence,
+            'work_experience' => $this->faker->sentence,
+            'organization_name' => $this->faker->sentence,
+            'position_duties' => $this->faker->sentence,
+            'salary' => $this->faker->sentence,
+            'resignation_reason' => $this->faker->sentence,
+            'achievements' => $this->faker->sentence,
+            'experience_gained' => $this->faker->sentence,
+            'talent' => $this->faker->sentence,
+            'reference_person_name' => $this->faker->sentence,
+            'reference_person_lastname' => $this->faker->sentence,
+            'current_position' => $this->faker->sentence,
+            'workplace_and_phone' => $this->faker->sentence,
+            'relationship' => $this->faker->sentence,
+            'signature_applicant' => $this->faker->sentence,
+            'submit_date' => $this->faker->sentence,
+            'signature_recruiter' => $this->faker->sentence,
+            'recruiter_date' => $this->faker->sentence,
+            'signature_payee' => $this->faker->sentence,
+            'payee_date' => $this->faker->sentence,
+            'accepted' => $this->faker->sentence,
+     
+        ];
+    }
+}
