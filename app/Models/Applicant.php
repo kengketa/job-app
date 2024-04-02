c3ea2d82f4ab29ff952303bf6827e478fa840fc5
kthitirat <kthitirat@aru.ac.th>
2024-04-02 18:49:59 +0700

    feat: Applicant model

--- app/Models/Applicant.php
+++ app/Models/Applicant.php
@@ -4,6 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Spatie\Image\Manipulations;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class Applicant extends Model
 {
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+       
         $this->call(RoleSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(AnnouncementTypeSeeder::class);
         $this->call(AnnouncementCategorySeeder::class);
         $this->call(AnnouncementSeeder::class);
+        $this->call(ApplicantSeeder::class);
         
     }
 }
